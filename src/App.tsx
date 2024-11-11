1466656484b97e42ae419664ee4f528fcabceefa
Caique Guedes <caique.guedes@estudante.ufjf.br>
2024-11-11 20:48:31 -0300

    Adiciona props

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ export default function App() {
   return(
     <>
       <h1>Olá, world!</h1> 
-       <Disciplina />
+       <Disciplina codigo="DCC207" nome="DESENVOLVIMENTO WEB I"/>
+       <Disciplina codigo="DCC207" nome="DESENVOLVIMENTO WEB II"/>
+       <Disciplina codigo="DCC207" nome="DESENVOLVIMENTO WEB FRONT-END"/>
     </>
   );
    
